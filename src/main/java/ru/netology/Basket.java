eecfdec0b6dbb321383482373ecc9a37b06a5659
KoshkaName <114123386+KoshkaName@users.noreply.github.com>
2022-12-26 16:43:24 +1000

    Main added

---
+++ src/main/java/ru/netology/Basket.java
@@ -0,0 +1,12 @@
+package ru.netology;
+
+public class Basket {
+
+    protected String title;
+    protected int count;
+
+    public Basket(String title, int count) {
+        this.title = title;
+        this.count = count;
+    }
+}
---
+++ src/main/java/ru/netology/Purchase.java
@@ -0,0 +1,38 @@
+package ru.netology;
+
+import java.util.Map;
+
+public class Purchase {
+
+    protected Basket[] purchases;
+
+    public Purchase(int size) {
+        purchases = new Basket[size];
+    }
+
+    public void addPurchase(String title, int count) {
+        for (int i = 0; i < purchases.length; i++) {
+            if (purchases[i] == null) {
+                purchases[i] = new Basket(title, count);
+                return;
+            }
+            if (purchases[i].title.equals(title)) {
+                purchases[i].count += count;
+                return;
+            }
+        }
+    }
+
+    public long sum(Map<String, Integer> prices) {
+        long sum = 0;
+        System.out.println("КОРЗИНА:");
+        for (int i = 0; i < purchases.length; i++) {
+            Basket purchase = purchases[i];
+            if (purchase == null) continue;
+            System.out.println("\t" + purchase.title + " " + purchase.count + " шт. в сумме " + (purchase.count * prices.get(purchase.title)) + " руб.");
+            sum += purchase.count * prices.get(purchase.title);
+        }
+        return sum;
+    }
+}
+
