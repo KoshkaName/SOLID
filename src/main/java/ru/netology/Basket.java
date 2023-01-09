8c94d80a36145a10a03fabfe7253ef7b175385a6
KoshkaName <114123386+KoshkaName@users.noreply.github.com>
2023-01-09 13:09:26 +1000

    Main added

--- src/main/java/ru/netology/Basket.java
+++ src/main/java/ru/netology/Basket.java
@@ -1,12 +1,37 @@
 package ru.netology;
 
+import java.util.Map;
+
 public class Basket {
 
-    protected String title;
-    protected int count;
+    protected Purchase[] purchases;
 
-    public Basket(String title, int count) {
-        this.title = title;
-        this.count = count;
+    public Basket(int size) {
+        purchases = new Purchase[size];
+    }
+
+    public void addPurchase(String title, int count) {
+        for (int i = 0; i < purchases.length; i++) {
+            if (purchases[i] == null) {
+                purchases[i] = new Purchase(title, count);
+                return;
+            }
+            if (purchases[i].title.equals(title)) {
+                purchases[i].count += count;
+                return;
+            }
+        }
+    }
+
+    public long sum(Map<String, Integer> prices) {
+        long sum = 0;
+        System.out.println("КОРЗИНА:");
+        for (int i = 0; i < purchases.length; i++) {
+            Purchase purchase = purchases[i];
+            if (purchase == null) continue;
+            System.out.println("\t" + purchase.title + " " + purchase.count + " шт. в сумме " + (purchase.count * prices.get(purchase.title)) + " руб.");
+            sum += purchase.count * prices.get(purchase.title);
+        }
+        return sum;
     }
 }
--- src/main/java/ru/netology/Purchase.java
+++ src/main/java/ru/netology/Purchase.java
@@ -4,35 +4,12 @@ import java.util.Map;
 
 public class Purchase {
 
-    protected Basket[] purchases;
+    protected String title;
+    protected int count;
 
-    public Purchase(int size) {
-        purchases = new Basket[size];
-    }
-
-    public void addPurchase(String title, int count) {
-        for (int i = 0; i < purchases.length; i++) {
-            if (purchases[i] == null) {
-                purchases[i] = new Basket(title, count);
-                return;
-            }
-            if (purchases[i].title.equals(title)) {
-                purchases[i].count += count;
-                return;
-            }
-        }
-    }
-
-    public long sum(Map<String, Integer> prices) {
-        long sum = 0;
-        System.out.println("КОРЗИНА:");
-        for (int i = 0; i < purchases.length; i++) {
-            Basket purchase = purchases[i];
-            if (purchase == null) continue;
-            System.out.println("\t" + purchase.title + " " + purchase.count + " шт. в сумме " + (purchase.count * prices.get(purchase.title)) + " руб.");
-            sum += purchase.count * prices.get(purchase.title);
-        }
-        return sum;
+    public Purchase(String title, int count) {
+        this.title = title;
+        this.count = count;
     }
 }
 
